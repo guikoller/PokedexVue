19f3277069b340fe4d7cebf08c2a3ee31c09d601
Guilherme Koller <g_koller@hotmail.com>
2021-07-13 09:11:05 -0300

    leftpanel and update and list creation

--- pokedex/src/components/LeftPanel/LeftPanelContent.vue
+++ pokedex/src/components/LeftPanel/LeftPanelContent.vue
@@ -1,13 +1,26 @@
 <template>
-  
+  <div class="content">
+      <List/>
+  </div>
 </template>
 
 <script>
-export default {
+import List from '@/components/List/List.vue'
 
+export default {
+    name: 'LeftPanelContent',
+    components: {
+        List,
+    },
 }
 </script>
 
 <style>
+    .content{
+        width: 100%;
 
+        @media (min-width: $viewport-medium) {
+			margin-top: 50px
+		}
+    }
 </style>
