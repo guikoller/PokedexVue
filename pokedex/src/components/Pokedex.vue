7e2243d0137eda2f7bc96523f31592bf214b48c3
Guilherme Koller <g_koller@hotmail.com>
2021-07-13 09:01:52 -0300

    creation of leftpanel and leftpanel content

--- pokedex/src/components/Pokedex.vue
+++ pokedex/src/components/Pokedex.vue
@@ -1,22 +1,22 @@
 <template>
     <div class="pokedex">
         <TopPanel />
-        <!-- <LeftPanel class="pokedex--left" />
-        <Divider />
+        <LeftPanel class="pokedex--left" />
+        <!-- <Divider />
         <RightPanel class="pokedex--right" /> -->
     </div>
 </template>
 
 <script>
     import TopPanel from './TopPanel/TopPanel.vue';
-    /* import LeftPanel from '@/components/LeftPanel/LeftPanel.vue';
-    import Divider from '@/components/Divider/Divider.vue';
+    import LeftPanel from '@/components/LeftPanel/LeftPanel.vue';
+    /* import Divider from '@/components/Divider/Divider.vue';
     import RightPanel from '@/components/RightPanel/RightPanel.vue'; */
 export default {
     name: 'Pokedex',
     components:{
         TopPanel,
-        //LeftPanel,
+        LeftPanel,
         //Divider,
         //RightPanel,
     },
