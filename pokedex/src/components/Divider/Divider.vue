4ab3b2bfb794eb4980bb8f9f61a11cf4ca2c932c
Guilherme Koller <g_koller@hotmail.com>
2021-07-13 13:58:07 -0300

    divider created

---
+++ pokedex/src/components/Divider/Divider.vue
@@ -0,0 +1,16 @@
+<template functional>
+	<divi class="divider">
+		<div class="arc--top"></div>
+		<div class="arc--bottom"></div>
+	</divi>
+</template>
+
+<script>
+export default {
+	name: 'Divider',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pokedex/src/components/LeftPanel/LeftPanel.vue
+++ pokedex/src/components/LeftPanel/LeftPanel.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="panel bg--red-01">
-      <LeftPanelContent/>
+      <LeftPanelContent />
   </div>
 </template>
 
