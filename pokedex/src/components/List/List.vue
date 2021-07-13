c66ce063a2671c50bed649502fa04a045da608e2
Guilherme Koller <g_koller@hotmail.com>
2021-07-13 09:19:42 -0300

    List style update

--- pokedex/src/components/List/List.vue
+++ pokedex/src/components/List/List.vue
@@ -1,13 +1,34 @@
 <template>
-  
+  <ul class="list text--white"/>
 </template>
 
 <script>
 export default {
-
+    name: 'List',
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+    .list{
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        height: 100%;
+        border: 20px solid cornflowerblue(white);
+        border-radius: 8px;
+        padding: 0 8px 8px 0;
+        overflow-y: scroll;
+        
+        /* Hide scroll bar */
+        -ms-overflow-style: none;
+        scrollbar-width: none;
+        &::-webkit-scrollbar{
+            display: none;
+        }
+        /*  */
 
+        @media (min-width: $viewport-medium) {
+			max-height: 72%;
+		}
+    }
 </style>
