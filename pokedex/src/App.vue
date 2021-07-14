b64e90825b8f81304ac5e8373d2e6048107c4848
Guilherme Koller <g_koller@hotmail.com>
2021-07-14 16:00:35 -0300

    right panel created and style bugs fixed

--- pokedex/src/App.vue
+++ pokedex/src/App.vue
@@ -5,10 +5,11 @@
 </template>
 
 <script>
-import Pokedex from './components/Pokedex.vue';
+	import Pokedex from './components/Pokedex.vue';
+
 	export default {
 		name: 'App',
-		components:{
+		components: {
 			Pokedex,
 		},
 	};
@@ -20,8 +21,7 @@ import Pokedex from './components/Pokedex.vue';
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-		height: calc(100vh -20px);
-		margin: 0 auto;
+		height: calc(100vh - 20px);
 		padding: 20px;
 	}
 </style>
